7268a11bf45ed509f77f58e492f6145fd03a2067
MOHAMED NACEUR BOUHAMED <mbouhamed@dom.tti>
2018-03-02 15:10:31 +0100

    first commit

---
+++ models/Patient.ts
@@ -0,0 +1,12 @@
+import { Record } from "./Record";
+
+export class Patient {
+
+    firstName: string;
+    lastName: string;
+    birthdate: string;
+    id: number;
+    address:string;
+    records:Record[];
+
+}
---
+++ models/Record.ts
@@ -0,0 +1,5 @@
+export class Record {
+    date:string;
+    HealthcareName:string;
+    description:string;
+}
