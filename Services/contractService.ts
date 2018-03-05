323d1bc76546afc8dfd816cf1521cb1c5ffcc47c
NaceurBouhamed <naceur.bouhamed@gmail.com>
2018-03-05 11:16:19 +0100

    refractor

--- Services/contractService.ts
+++ Services/contractService.ts
@@ -1,5 +1,7 @@
 import Web3 = require('web3');
 
+import dotnv = require('dotenv');
+
 
 export class ContractService {
 
@@ -7,7 +9,7 @@ export class ContractService {
 
     hblockContract: any;
 
-    hblockDAO: any;
+    hblockService: any;
 
     GasPrice: any;
 
@@ -19,12 +21,26 @@ export class ContractService {
 
         this.hblockContract = this.web3.eth.contract([{ "constant": true, "inputs": [{ "name": "", "type": "address" }], "name": "patients", "outputs": [{ "name": "", "type": "bool" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [{ "name": "patient", "type": "address" }], "name": "grantAccess", "outputs": [{ "name": "", "type": "bool" }], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": false, "inputs": [], "name": "addClinic", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [{ "name": "", "type": "address" }], "name": "records", "outputs": [{ "name": "", "type": "bytes32" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [{ "name": "doc", "type": "address" }], "name": "addDoctor", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": false, "inputs": [{ "name": "patient", "type": "address" }], "name": "getRecord", "outputs": [{ "name": "", "type": "bytes32" }], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [{ "name": "", "type": "address" }], "name": "clinics", "outputs": [{ "name": "", "type": "bool" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [{ "name": "patient", "type": "address" }, { "name": "hsh", "type": "bytes32" }], "name": "appendToRecord", "outputs": [{ "name": "", "type": "bool" }], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": false, "inputs": [{ "name": "patient", "type": "address" }], "name": "addPatient", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [{ "name": "", "type": "address" }], "name": "doctors", "outputs": [{ "name": "", "type": "address" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [{ "name": "patient", "type": "address" }], "name": "forceGetRecord", "outputs": [{ "name": "", "type": "bytes32" }], "payable": false, "stateMutability": "nonpayable", "type": "function" }]);
 
-        this.hblockDAO = this.hblockContract.at('0x2e4347836005cebeed350f0f11133d166c8eb236');
+        this.hblockService = this.hblockContract.at('0x2e4347836005cebeed350f0f11133d166c8eb236');
 
         this.GasPrice = this.web3.toWei(0.00001, "ether");
 
     }
 
+    newWallet = () => {
+        let acc = this.web3.eth.accounts.create();
+        this.web3.eth.sendTransaction({from:"none", to: "non" ,value: this.web3.toWei(0.01, "ether")})
+        return (acc.address);
+    }
+
+    addDoctor = (fromA:string,address:string) => {
+        this.hblockService.addDoctor.sendTransaction(address, { from:fromA, gas:34774, gasPrice:this.GasPrice
+    },function(err,res){ if(!err) console.log(res)}
+    )
+
+
+
+    }
 
 
 }
